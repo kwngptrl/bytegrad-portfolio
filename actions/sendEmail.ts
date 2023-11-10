6b1946c0e51b2db168e24eb6460cf23e2d27af02
ptr kwng <ptrlkwng@hotmail.com>
2023-11-10 17:44:10 +0800

    first commit, ran next build

---
+++ actions/sendEmail.ts
@@ -0,0 +1,66 @@
+"use server";
+
+import React from "react";
+import { Resend } from "resend";
+import { validateString, getErrorMessage } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
+
+const resend = new Resend(process.env.ORIG_RESEND_API_KEY);
+/* 
+const getErrorMessage = (error: unknown): string => {
+    let message: string;
+
+    if (error instanceof Error) {
+        message = error.message;
+    } else if (error && typeof error === 'object' && 'message' in error) {
+        message = String(error.message);
+    } else if (typeof error === "string") {
+        message = error;
+    } else {
+        message = "Something went wrong";
+    }
+    return message;
+} */
+
+export const sendEmail = async (formData: FormData) => {
+    /* console.log("Running on server");
+    console.log(formData.get("senderEmail"));
+    console.log(formData.get("message")); */
+    const senderEmail = formData.get('senderEmail');
+    const message = formData.get('message');
+
+    /* simple server-side validation */
+    if (!validateString(senderEmail, 500)) {
+        return {
+            error: "Invalid sender email",
+        };
+    }
+    if (!validateString(message, 5000)) {
+        return {
+            error: "Invalid message",
+        };
+    }
+
+    let data;
+    try {
+        await resend.emails.send({
+            from: "Contact Form <onboarding@resend.dev>",
+            to: "tact41524@gmail.com",
+            subject: "Message from contact form today Nov. 2, 2023",
+            reply_to: senderEmail as string,
+            /* text: message as string, */
+            /* react: <ContactFormEmail message={message} senderEmail={senderEmail} /> */
+            react: React.createElement(ContactFormEmail, {
+                message: message as string,
+                senderEmail: senderEmail as string,
+            })
+        });
+    } catch (error: unknown) {
+        return {
+            error: getErrorMessage(error)
+        }
+    }
+    return {
+        data,
+    };
+}
---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='mb-10 px-4 text-center text-gray-500'>
+        <small className='mb-2 block test-xs'>
+            &copy; 20230 Ricardo. All rights reserved.
+            <p className='text-xs'>
+                <span className='font-semibold'>About this website:</span> built with React & Next.js (App Router & Server Actions), Typescript, Tailwind CSS, Framer Motion, React Email, Resend, Vercel hosting.
+            </p>
+        </small>
+    </footer>
+  )
+}
